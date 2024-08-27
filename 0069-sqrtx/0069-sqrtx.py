d5fb72b2736d5176138308dd0413b6c278dbe3fa
agrawalaayush <agrawalaayush1993@gmail.com>
2024-08-27 20:08:27 +0530

    Time: 1313 ms (8.26%), Space: 11.6 MB (30.94%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        i = 0 
+        while True:
+            if i * i > x:
+                return i - 1
+            i = i + 1
+            
+        
